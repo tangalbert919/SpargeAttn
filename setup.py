f8a1db97b8e235da1768cccd546e93992700daf4
Albert Tang <tangalbert919@yahoo.com>
2025-09-19 09:40:08 -0500

    Pass "-DUSE_CUDA" on Windows
    Gets past ambiguous std error when compiling, and torch.compile isn't
    supported on Windows anyway.

--- setup.py
+++ setup.py
@@ -68,6 +68,9 @@ ABI = 1 if torch._C._GLIBCXX_USE_CXX11_ABI else 0
 CXX_FLAGS += [f"-D_GLIBCXX_USE_CXX11_ABI={ABI}"]
 NVCC_FLAGS_COMMON += [f"-D_GLIBCXX_USE_CXX11_ABI={ABI}"]
 
+if os.name == "nt":
+    NVCC_FLAGS_COMMON += ["-DUSE_CUDA"]
+
 if CUDA_HOME is None:
     raise RuntimeError(
         "Cannot find CUDA_HOME. CUDA must be available to build the package.")
